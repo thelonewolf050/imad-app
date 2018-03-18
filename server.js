9cab2b0031e00fe48560ac1f14fe2bc3b9c1cb58
thelonewolf050 <35637047+thelonewolf050@users.noreply.github.com>
2018-03-18 17:01:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config = {
 	database: 'nikhilsingh050',
 	host:'db.imad.hasura-app.io',
 	port: '5432',
-	password: process.env.DB_PASSWPRD
+	password: process.env.DB_PASSWORD
 };
 
 var pool = new Pool(config);
