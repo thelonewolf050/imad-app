c5fb7a35cae08bccb3be671c997268d8b9cae9e1
thelonewolf050 <35637047+thelonewolf050@users.noreply.github.com>
2018-03-18 20:47:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,29 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //create request object
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange= function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            
+            if(request.status === 200) {
+                alert("Logged in successfully");
+            }
+            else if (request.status === 403) {
+                alert("Username or pass incorrect");
+            }
+            else if(request.status === 500) {
+                alert("Something went wrong on the server");
+            }
+        }
+    };
+  
+    //MAKE a REQUEST
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(password);
+    console.log(username);
+    request.open('POST', 'http://nikhilsingh050.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
