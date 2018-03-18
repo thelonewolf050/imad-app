20ea06fa7b5f3c1775bdf9ed86d722d8e49d4c02
thelonewolf050 <35637047+thelonewolf050@users.noreply.github.com>
2018-03-18 18:46:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,10 +76,15 @@ app.get('/articles/:articleName', function(req, res) {
 	});
 });
 
-app.get('/hash/:input', function(req, res){
-    
-    ashedString = 
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
 }
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input, 'this-is-a-random-string');
+    res.send(hashedString);
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
