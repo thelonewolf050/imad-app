8d29c92e52ec2ace7d4688634b8913f97f2eec6c
thelonewolf050 <35637047+thelonewolf050@users.noreply.github.com>
2018-03-18 16:12:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,6 +42,24 @@ function createTemplate(data) {
     return htmlTemplate;
 }
 
+app.get('/articles/:articleName', function(req, res) {
+	pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
+		if (err) {
+			res.status(500).send(err.toString());
+		}
+		else {
+			if(result.rows.length === 0) {
+				res.status(404).send('Article not found');
+
+			}
+			else {
+				var articleData = result.rows[0];
+				res.send(createTemplate(articleData));
+			}
+		}
+	});
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
